40388c95acdea52368c80689585e202aa323adf1
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-03-18 19:06:47 -0700

    header

---
+++ app/theme.js
@@ -0,0 +1,18 @@
+export const themes = {
+  colors: {
+    0: "#717C89",
+    1: "#8AA2A9",
+    2: "#90BAAD",
+    3: "#A1E5AB",
+    4: "#ADF6B1",
+  },
+};
+
+/* 
+Color Templates
+#717C89 slate gray
+#8AA2A9 cadet gray
+#90BAAD cambridge blue
+#A1E5AB celadon
+#ADF6B1 light green
+ */
---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import DarkModeToggle from "./DarkModeToggle";
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
+      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
+        <div>Home</div>
+        <div>Popular Shows</div>
+        <div>Search Bar</div>
+        <div className="flex-1 flex items-center justify-end space-x-4">
+          <div>Favorites</div>
+          <div>Menu Bar</div>
+          <DarkModeToggle />
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;

cb5a15d01655ee5f716c5952406456cef4ede32b
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-03-21 20:49:20 -0700

    update with buttons for selectedShow, and display rendering -button corners are roudned -button color changes with selected

--- components/SelectedShowDisplay.tsx
+++ components/SelectedShowDisplay.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { Button } from "./ui/button";
 import ImageDisplay from "./ImageDisplay";
+import DisplayButtons from "./DisplayButtons";
 
 type Episode = {
   episodeNumber: number;
@@ -16,12 +17,19 @@ type Props = {
   showData: (Episode[] | undefined)[];
 };
 const SelectedShowDisplay = ({ showData }: Props) => {
-  const [currentSelectedEpisode, setCurrentSelectedEpisode] = useState(
-    showData?.[0]?.[0]
-  );
+  const [seasonSelection, setSeasonSelection] = useState(0);
+  const [episodeSelection, setEpisodeSelection] = useState(0);
+
+  const [currentSelectedEpisode, setCurrentSelectedEpisode] = useState<
+    Episode | undefined
+  >(showData?.[seasonSelection]?.[episodeSelection]);
+
+  const changeSeasonSelection = (newSeason: number) => () => {
+    setSeasonSelection(newSeason);
+  };
 
   return (
-    <div className="flex justify-center items-center flex-col mt-48">
+    <div className="flex justify-center items-center flex-col mt-12">
       <ImageDisplay
         source={currentSelectedEpisode?.still_path}
         name={currentSelectedEpisode?.episodeTitle}
@@ -30,8 +38,31 @@ const SelectedShowDisplay = ({ showData }: Props) => {
       <h1>Episode #</h1>
 
       <Button>Generate Random Episode</Button>
-      <h1>Number of Seasons</h1>
-      {showData.map((data, i) => i + 1)}
+
+      <h1 className="my-10 font-black text-4xl">Seasons</h1>
+      <div className="flex ">
+        {showData.map((data, i) => (
+          <div key={i} className="mx-2">
+            <Button
+              key={i}
+              variant={i === seasonSelection ? "selected" : "secondary"}
+              onClick={changeSeasonSelection(i)}
+            >
+              {i + 1}
+            </Button>
+          </div>
+        ))}
+      </div>
+      <h1 className="my-10 font-black text-4xl">Episodes</h1>
+      <div className="flex ">
+        {showData[seasonSelection]?.map((data, i) => (
+          <div key={i} className="mx-2">
+            <Button key={i} variant="secondary">
+              {i + 1}
+            </Button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- components/TVListDisplay.tsx
+++ components/TVListDisplay.tsx
@@ -5,14 +5,6 @@ import React from "react";
 import ImageDisplay from "./ImageDisplay";
 
 function TVListDisplay({ shows }: { shows: PopularShows[] }) {
-  const getImagePath = (imagePath?: string, fullSize?: boolean) => {
-    return imagePath
-      ? `http://image.tmdb.org/t/p/${
-          fullSize ? "original" : "w500"
-        }/${imagePath}`
-      : "https://links.papareact.com/o8z";
-  };
-
   return (
     <div
       style={{

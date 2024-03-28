c2f82119ed03917f121666330e8a5f61083ed28c
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-03-27 21:00:43 -0700

    updates with homepage introduction

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="flex items-center justify-center">
+      <div className="w-full max-w-5xl px-4 text-center mt-[10%]">
+        <h1 className="font-bold mb-6 text-4xl">
+          Hello, Welcome to Episode Roulette
+        </h1>
+        <p className="text-gray-500 leading-relaxed">
+          This website was inspired by my need to find a random episode in my
+          favorite tv shows. I found myself wanting to watch a random episode,
+          only from a couple of selection seasons, depending on my mood. Instead
+          of having to go to a generic random episode generator and waiting
+          until something that I was looking for came up, I was able for someone
+          to make a decision that I agreed with!
+        </p>
+      </div>
+    </main>
+  );
 }

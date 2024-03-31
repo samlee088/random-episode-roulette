128dff588040ba9f13c59cb2661b0a67543a1fcb
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-03-30 18:31:47 -0700

    able to connect users to fiebase, however unable to get userID, only session data

--- auth.ts
+++ auth.ts
@@ -1,5 +1,8 @@
+import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { adminAuth, adminDB } from "./firebase-admin";
+import { Adapter } from "next-auth/adapters";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -8,7 +11,28 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        if (token.sub) {
+          session.user.id = token.sub;
+
+          const firebaseToken = await adminAuth.createCustomToken(token.sub);
+          session.firebaseToken = firebaseToken;
+        }
+      }
+
+      return session;
+    },
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.sub = user.id;
+      }
+      return token;
+    },
+  },
   session: {
-    strategy: 'jwt',
-  }
+    strategy: "jwt",
+  },
+  adapter: FirestoreAdapter(adminDB) as Adapter,
 } satisfies NextAuthOptions;
